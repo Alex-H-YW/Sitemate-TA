6ab6576c996d3f066b3e6bde8c0e6fc19ff3f8fd
Alex Huang <alexhhhxx@gmail.com>
2022-10-28 14:41:05 +1100

    add js and css

---
+++ index.js
@@ -0,0 +1,7 @@
+const clickmeBtn = document.querySelector(".submitBtn");
+const modal = document.getElementById("modal");
+const yesButton = document.querySelector(".yesBtn");
+const noButton = document.querySelector(".noBtn");
+const colseButton = document.querySelector(".closeBtn");
+const modalEl = document.querySelector(".modalTitle");
+const overlay = document.querySelector(".overlay");
